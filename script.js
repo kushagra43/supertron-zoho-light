ee97900babfcd4cb903ec7701c0350234c633768
kushagra43 <63740432+kushagra43@users.noreply.github.com>
2023-10-12 13:35:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+document.querySelector("#myForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  // Collect form data
+  let formData = new FormData(this);
+
+  const number = formData.get("mobilephone");
+
+  var expr = /^(0|91)?[6-9][0-9]{9}$/;
+  if (!expr.test(number)) {
+    document.querySelector("#mobileError").innerHTML =
+      "Please enter a valid 10 digit mobile number";
+    document.querySelector("#mobileError").style.color = "red";
+    document.querySelector("#mobileError").style.display = "block";
+    return;
+  }
+
+  // Send post request to the server
+  fetch(
+    "https://forms.hubspot.com/uploads/form/v2/23736002/5f288f3f-b0e0-4f28-96d4-fc665ceeea3d",
+    {
+      method: "POST",
+      body: formData,
+    }
+  )
+    .then((response) => {
+      if (response.status >= 200 && response.status < 300) {
+        // Show success message
+        document.querySelector("#thankYou").innerHTML =
+          "Thank you for submitting the form!";
+        document.querySelector("#thankYou").style.color = "green";
+        document.querySelector("#thankYou").style.display = "block";
+      } else {
+        // Show error message
+        document.querySelector("#thankYou").innerHTML =
+          "An error occurred while submitting the form. Please try again later.";
+        document.querySelector("#thankYou").style.color = "red";
+        document.querySelector("#thankYou").style.display = "block";
+      }
+
+      // Remove form
+      this.remove();
+    })
+    .catch((error) => {
+      console.error(error);
+      // Show error message
+      document.querySelector("#thankYou").innerHTML =
+        "Something went wrong, please try again later.";
+      document.querySelector("#thankYou").style.color = "red";
+      document.querySelector("#thankYou").style.display = "block";
+      // Remove form
+      this.remove();
+    });
+});
+
+// Slider Movements
+const moveSliderToLeft = () => {
+  const scrollableElement = document.querySelector("#sliderCont");
+
+  const screenWidth = window.innerWidth;
+  const scrollAmount = screenWidth < 760 ? 280 : 400;
+
+  scrollableElement.scrollBy({
+    left: scrollAmount,
+    behavior: "smooth",
+  });
+};
+
+const moveSliderToRight = () => {
+  const scrollableElement = document.querySelector("#sliderCont");
+
+  const screenWidth = window.innerWidth;
+  const scrollAmount = screenWidth < 760 ? -280 : -400;
+
+  scrollableElement.scrollBy({
+    left: scrollAmount,
+    behavior: "smooth",
+  });
+};
